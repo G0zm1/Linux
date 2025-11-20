0c36006b72c622285e69f298582aaeef47bd4803
Dennis Voigt <dennisvoigt1995@gmail.com>
2025-11-20 10:28:48 +0100

    Update Aufgabe1.c

--- C/Vater_Sohn_Schleife/Aufgabe1.c
+++ C/Vater_Sohn_Schleife/Aufgabe1.c
@@ -20,13 +20,14 @@ int main() {
   }
 
   else {
+    
+    wait(NULL);
+
     for (int i = 1; i <= 2000; i++) {
       if (i % 2 == 0) {
         printf("Das sind die geraden Zahlen vom Vaterprozess (PID %d): %d\n", getpid(), i);
       }
     }
-
-    wait(NULL);
   }
   return 0;
 }
